d8868c9394414e7134bc453b2e78593f6ac50fb7
mohanad <nalhaj401@gmail.com>
2026-05-18 01:55:19 +0300

    a new fun was added

--- try.cpp
+++ try.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+
+int test2(int a , int b){
+	int x;
+	x = a + b;
+}
+
 void test(){
 cout<<"test 1"<<endl;
 	
@@ -9,10 +15,12 @@ cout<<"test 1"<<endl;
 
 int main()
 {
+	
+	int a = 2 ,b = 3;
 	cout<<"hii"<<endl;
 	cout<<"ss"<<endl;
 	
-	
+	test2(a , b)
 	test();	
 	return 0;
 }
